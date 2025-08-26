9a42ce68560c4a1402c9b6173221550537d8d63e
vijay-murugan <vijaymurugan1457@gmail.com>
2025-08-26 15:00:36 -0500

    removed / from URL to address cors issue

--- controller/main_controller.py
+++ controller/main_controller.py
@@ -17,7 +17,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://resmatch.netlify.app/"
+        "https://resmatch.netlify.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
